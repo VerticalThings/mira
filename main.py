d1ad66f00f3d7e11405e3b5676b93cd59200c4e4
VerticalThings <111590392+VerticalThings@users.noreply.github.com>
2022-09-02 15:50:35 +0300

    for the server shit stuff ok

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for, request;
+import json;
+from cryptography.fernet import Fernet;
+app = Flask(__name__);
+
+@app.route("/miramarket/api/register", methods=["POST"])
+def signup():
+    postedform = request.form;
+    username = postedform["username"].lower()
+    password = postedform["password"]
+    verification = postedform["lowverify"]
+    if verification != "0481613180107414":
+    	return "You need an api key to get access to this."
+    else:
+    	pass
+    return postedform
+app.run();
